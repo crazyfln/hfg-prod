c17c10a13d2dea8dd67844934704c2d4f7c27a87
alligatortower <mrweed@gmail.com>
2014-05-21 05:49:56 -0400

    users can favorite a facility

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from decimal import *
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseBadRequest
 from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DetailView
 
 from django.contrib.auth.decorators import login_required
@@ -18,7 +20,9 @@ from .models import *
 
 @render_to('index.html')
 def index(request):
-    return {}
+    facilities = Facility.objects.all()
+    data = {'facilities':facilities}
+    return data
 
 def error(request):
     """for testing purposes"""
@@ -35,16 +39,30 @@ def favorites(request):
 def profile(request):
     raise Http404
 
-class Facility(DetailView):
+class FacilityDetail(DetailView):
     model = Facility
     template_name = 'facility_detail.html'
 
     def get_context_data(self, **kwargs):
-        context = super(Facility, self).get_context_data(**kwargs)
+        context = super(FacilityDetail, self).get_context_data(**kwargs)
         context['all_conditions'] = Condition.objects.all()
         context['all_amenities'] = Amenity.objects.all()
         context['all_languages'] = Language.objects.all()
         return context
+
+@login_required
+def facility_favorite(request, slug):
+    facility = get_object_or_404(Facility, slug=slug)
+    if request.user in facility.favorited_by.all():
+        favorite = Favorite.objects.get(user=request.user, facility=facility)
+        favorite.delete()
+    else:
+        favorite = Favorite(user=request.user, facility=facility)
+        favorite.save()
+
+    return redirect(request.GET['next'])
+
+
 @ajax_request
 @login_required
 def create_customer(request):

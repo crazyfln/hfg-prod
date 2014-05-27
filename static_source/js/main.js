c79038ee3a71b705951e568d07b438615a4d5c44
chris weed <chris@lightmatter.com>
2014-05-27 13:36:48 -0400

    "index listing boxes are wired"

--- static_source/js/main.js
+++ static_source/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.result-box').click(function() {
+    $('.listing-preview').click(function() {
         url = $(this).attr("url");
         window.location.href=url;
     });   

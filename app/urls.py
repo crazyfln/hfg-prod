85a49d5c2b35b01e0c421e37abab5241b1b35dad
Greg Hausheer <gregoryhausheer@gmail.com>
2014-06-16 11:13:37 -0400

    receive_reward page styling and partial new home page design without new facility preview

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('app.views',
     url(r'^contact/$', Contact.as_view(), name='contact'),
     url(r'^profile/$', Profile.as_view(), name='profile'),
     url(r'^create_customer/', 'create_customer', name='create_customer'),
+    url(r'^receive_reward/', 'receive_reward', name='receive_reward'),
     (r'^$', TemplateView.as_view(template_name="index.html")),
 )
 

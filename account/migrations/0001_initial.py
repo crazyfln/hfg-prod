9f0fa40adb81bc4768d9624fa219a6ee5493cb27
chris weed <chris@lightmatter.com>
2014-06-19 17:52:40 -0400

    derp

--- account/migrations/0001_initial.py
+++ account/migrations/0001_initial.py
@@ -4,11 +4,11 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
-needed_by = (
-        ("reversion", "0001_initial"),
-        ("app", "0001_initial"),
-)
 class Migration(SchemaMigration):
+    needed_by = (
+            ("reversion", "0001_initial"),
+            ("app", "0001_initial"),
+    )
 
     def forwards(self, orm):
         # Adding model 'User'

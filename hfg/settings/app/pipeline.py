15cd94cbac2db1757ba6242fc496d07d0a807968
Ben Beecher <benbeecher@gmail.com>
2014-06-22 18:09:56 -0400

    process of elimination

--- hfg/settings/app/pipeline.py
+++ hfg/settings/app/pipeline.py
@@ -32,6 +32,7 @@ PIPELINE_JS = {
     'vendor': {
         'source_filenames': (
             'js/vendor/jquery-1.11.1.min.js',
+            'js/vendor/bootstrap.js',
             'js/vendor/select2.js',
         ),
         'output_filename': 'js/vendor.js',

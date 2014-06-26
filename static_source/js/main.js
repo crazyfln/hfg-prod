c01ceda70373d2acf18c397a24111eb058904c61
chris weed <chris@lightmatter.com>
2014-06-26 16:36:46 -0400

    cleaning up from merge

--- static_source/js/main.js
+++ static_source/js/main.js
@@ -1,48 +1,39 @@
 $(document).ready(function(){
-  $('.listing-preview').click(function(e) {
-    url = $(this).attr("url");
-    window.location.href=url;
-  });
-
-  $('[id^=carousel-selector-]').click(function () {
-    var id_selector = $(this).attr("id");
-    var id = id_selector.substr(id_selector.length - 1);
-    id = parseInt(id);
-    $('#facility-carousel').carousel(id);
-    $('[id^=carousel-selector-]').removeClass('selected');
-    $(this).addClass('selected');
-  }); 
-
-
-  $('.right').click(function() {
-    $('.carousel').carousel('next');
-  });
-
-  $('.left').click(function() {
-    $('.carousel').carousel('prev');
-  });
-
-   $('.carousel').carousel({
-    pause: "hover"
-  });
-
-
-    // when the carousel slides, auto update
-    $('#facility-carousel').on('slid', function (e) {
-     var id = $('.item.active').data('slide-number');
-     id = parseInt(id);
-     $('[id^=carousel-selector-]').removeClass('selected');
-     $('[id^=carousel-selector-' + id + ']').addClass('selected');
-   });
-
-    $('.heart-holder').on('click', function(e) {
-        e.stopPropagation();
-        if ($(this).data('logged_in') == 'no'){
-            $('#Login-Modal').modal('show')
-        }
+    $('.listing-preview').click(function(e) {
+        url = $(this).attr("url");
+        window.location.href=url;
     });
 
-  });
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length - 1);
+        id = parseInt(id);
+        $('#facility-carousel').carousel(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $(this).addClass('selected');
+    }); 
+
+
+    $('.right').click(function() {
+        $('.carousel').carousel('next');
+    });
+
+    $('.left').click(function() {
+        $('.carousel').carousel('prev');
+    });
+
+    $('.carousel').carousel({
+        pause: "hover"
+    });
+
+
+        // when the carousel slides, auto update
+    $('#facility-carousel').on('slid', function (e) {
+        var id = $('.item.active').data('slide-number');
+        id = parseInt(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $('[id^=carousel-selector-' + id + ']').addClass('selected');
+    });
 
     $('.heart-holder').on('click', function(e) {
         e.preventDefault();
@@ -58,4 +49,8 @@ $(document).ready(function(){
                 },
             });
         }
+        else if ($(this).data('logged_in') == 'no'){
+            $('#Login-Modal').modal('show')
+        }
     });
+});

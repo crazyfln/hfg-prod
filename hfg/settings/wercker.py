7b402de276479391f9c778c7d3039e2e28209626
Ben Beecher <benbeecher@gmail.com>
2014-08-29 12:09:53 -0400

    trying CI with debug on

--- hfg/settings/wercker.py
+++ hfg/settings/wercker.py
@@ -6,7 +6,6 @@ import dj_database_url
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CONTACT_EMAIL = 'greg@lightmatter.com'
 
-DATABASES = {}
 DATABASES = {
     'default': dj_database_url.config(
         env="WERCKER_POSTGRESQL_URL",
@@ -21,13 +20,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = PROJECT_ROOT.child("static")
-STATIC_URL = '/static/'
 
-DEBUG = True
-PIPELINE_ENABLED = False
+DEBUG = False
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # CACHES = {
 #     'default': {

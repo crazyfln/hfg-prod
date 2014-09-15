09c2dfec2d4a5160626ec4b4e6bd9ff92bbe6c15
Ben Beecher <benbeecher@gmail.com>
2014-09-15 17:53:30 -0400

    fixing stupid bug

--- .profile.d/gis.sh
+++ .profile.d/gis.sh
@@ -1,2 +1,2 @@
 export LIBRARY_PATH=$LIBRARY_PATH:/app/.gis/proj4/lib:/app/.gis/gdal/lib:/app/.gis/geos/lib
-export LD_LIBRARY_PATH: $LD_LIBRARY_PATH:/app/.gis/proj4/lib:/app/.gis/gdal/lib:/app/.gis/geos/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.gis/proj4/lib:/app/.gis/gdal/lib:/app/.gis/geos/lib

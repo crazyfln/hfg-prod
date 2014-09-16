82ff0511c8f5f690e825c3ab0c9538db2e5bcd02
Ben Beecher <benbeecher@gmail.com>
2014-09-16 11:18:32 -0400

    adding in shell script to manually set library and ld_library paths

---
+++ .profile.d/gis.sh
@@ -0,0 +1,2 @@
+export LIBRARY_PATH=/app/.heroku/vendor/lib:/app/.heroku/python/lib:/app/vendor/geos/3.4.1/lib:/app/vendor/gdal/1.11.0/lib:/app/vendor/proj/4.8.0/lib
+export LD_LIBRARY_PATH=/app/.heroku/vendor/lib:/app/.heroku/python/lib:/app/vendor/geos/3.4.1/lib:/app/vendor/gdal/1.11.0/lib:/app/vendor/proj/4.8.0/lib

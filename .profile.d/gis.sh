4f750423174ccbc5490ee833874f84fe45994fd6
Ben Beecher <benbeecher@gmail.com>
2014-09-16 11:32:12 -0400

    moving post compile steps to after gis settings

--- .profile.d/gis.sh
+++ .profile.d/gis.sh
@@ -1,2 +1,5 @@
 export LIBRARY_PATH=/app/.heroku/vendor/lib:/app/.heroku/python/lib:/app/vendor/geos/3.4.1/lib:/app/vendor/gdal/1.11.0/lib:/app/vendor/proj/4.8.0/lib
 export LD_LIBRARY_PATH=/app/.heroku/vendor/lib:/app/.heroku/python/lib:/app/vendor/geos/3.4.1/lib:/app/vendor/gdal/1.11.0/lib:/app/vendor/proj/4.8.0/lib
+python manage.py collectstatic
+python manage.py syncdb
+python manage.py migrate --delete-ghost-migrations
